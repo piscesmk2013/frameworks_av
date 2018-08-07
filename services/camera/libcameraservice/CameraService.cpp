f10262c8d47bdafdcfa8e201106a0782b56e2e97
darkobas <darkobas@gmail.com>
2018-08-07 13:00:17 +0200

    libcameraservice: add TARGET_CAMERA_NEEDS_CLIENT_INFO
    Change-Id: I85c2e9d4ffb56cc08aea1c1eabc1ed2ec45c0217
    
    @SKULSHADY: Convert to soong config variable
    
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Change-Id: Id4fd49164c9a4f093e7decbd446a03aaf5369507
    (cherry picked from commit 6ef53a2253794b2ab2f31cf74d263bbccd202e44)

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -25,6 +25,10 @@
 #include <cstring>
 #include <ctime>
 #include <string>
+#ifdef TARGET_NEEDS_CLIENT_INFO
+#include <iostream>
+#include <fstream>
+#endif
 #include <sys/types.h>
 #include <inttypes.h>
 #include <pthread.h>
@@ -3268,9 +3272,15 @@ status_t CameraService::BasicClient::startCameraOps() {
 
     // Notify listeners of camera open/close status
     sCameraService->updateOpenCloseStatus(mCameraIdStr, true/*open*/, mClientPackageName);
-#ifdef CAMERA_NEEDS_CLIENT_INFO_LIB
-    gVendorCameraProviderService->setPackageName(String8(mClientPackageName).string());
-#endif
+	#ifdef TARGET_NEEDS_CLIENT_INFO
+    std::ofstream cpf("/data/misc/aosp/client_package_name");
+    std::string cpn = String8(mClientPackageName).string();
+    cpf << cpn;
+	#endif
+
+	#ifdef CAMERA_NEEDS_CLIENT_INFO_LIB
+	    gVendorCameraProviderService->setPackageName(String8(mClientPackageName).string());
+	#endif
     return OK;
 }
 
