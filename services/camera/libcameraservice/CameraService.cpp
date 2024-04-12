3f78b48b0ca1c27d6dc1e2b227ba465048a71203
piscesmk2013 <piscesmk2013@gmail.com>
2024-04-12 21:32:20 +0800

    fixup!cameraserver: Allow google camera to access multiple cameras

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -1911,7 +1911,7 @@ void CameraService::finishConnectLocked(const sp<BasicClient>& client,
                     oomScoreOffset, systemNativeClient);
     auto evicted = mActiveClientManager.addAndEvict(clientDescriptor);
 
-    const char* packageName = String8(client->getPackageName()).string();
+    const char* packageName = toString8(client->getPackageName()).c_str();
 
     if (strcmp(packageName, "com.android.camera") == 0 
         || strcmp(packageName, "com.google.android.GoogleCamera") == 0) {
@@ -2042,7 +2042,7 @@ status_t CameraService::handleEvictionsLocked(const std::string& cameraId, int c
         // Find clients that would be evicted
         auto evicted = mActiveClientManager.wouldEvict(clientDescriptor);
 
-        const char* packageNameStr = String8(packageName).string();
+        const char* packageNameStr = toString8(packageName).c_str();
 
         if (strcmp(packageNameStr, "com.android.camera") == 0 
             || strcmp(packageNameStr, "com.google.android.GoogleCamera") == 0) {
